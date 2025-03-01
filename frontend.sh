26533c40ffbb19e9a6a7a6b9b049e8a630785274
balaguru-20 <balaguravaiah0606@gmail.com>
2025-03-01 17:39:34 +0530

    backend shell script

---
+++ frontend.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"  #Red
+G="\e[32m"  #Green
+Y="\e[33m"  #Yellow
+N="\e[0m"   #Normal
+
+LOGS_FOLDER="/var/log/expense-logs" # have to create like this in linux $ sudo mkdir -p /var/log/expense-logs
+LOG_FILE=$(echo $0 | cut -d "." -f1 )   # It wll takes 13-logs.sh file name before .(dot)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){             #We can write the function anywhere in the program
+    if [ $1 -ne 0 ]   
+    then
+        echo -e "$2 ... $R FAILURE $N" #-e means enable, $R color cpplying
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e " $R ERROR:: you must have sudo access to execute this script $N"
+        exit 1      #Other than 0
+    fi
+    }
+
+mkdir -p $LOGS_FOLDER &>>$LOG_FILE_NAME
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT
+
+dnf install nginx -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing Nginx server"
+
+systemctl enable nginx &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling Nginx server"
+
+systemctl start nginx &>>$LOG_FILE_NAME
+VALIDATE $? "Starting Nginx Server"
+
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE_NAME
+VALIDATE $? "Removing existing version of the code"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE_NAME
+VALIDATE $? "Downloading latest code"
+
+cd /usr/share/nginx/html
+VALIDATE $? "Moving to HTML directory"
+
+unzip /tmp/frontend.zip &>>$LOG_FILE_NAME
+VALIDATE $? "unziping the frontend of the code"
+
+systemctl restart nginx &>>$LOG_FILE_NAME
+VALIDATE $? "Restarting Nginx"
