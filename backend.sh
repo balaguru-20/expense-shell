b1f87a687466b4be20241a3d1f5c0f456d2d7f6d
balaguru-20 <balaguravaiah0606@gmail.com>
2025-03-01 16:57:27 +0530

    backend shell script

--- backend.sh
+++ backend.sh
@@ -34,14 +34,12 @@ echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 CHECK_ROOT
 
-expense_logs=find /var/log/expense-logs/ -type d -name "expense-logs"   &>>$LOG_FILE_NAME
-
-if [ $expense_logs -ne expense-logs ]
+if [ ! -d "/var/log/expense-logs/" ]
 then
     mkdir /var/log/expense-logs &>>$LOG_FILE_NAME
     VALIDATE $? "creating expense-logs directory"
 else
-    echo -e "$expense_logs already created --- $Y SKIPPING $N"
+    echo -e "expense_logs already created --- $Y SKIPPING $N"
 fi
 
 dnf module disable nodejs -y  &>>$LOG_FILE_NAME
